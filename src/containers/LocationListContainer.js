007c31c669c2a0aec65a5ff51bbf4228af3f3781
Poty <rivadeneiraie@gmail.com>
2020-05-11 19:30:58 -0300

    139 BindActionCreators

--- src/containers/LocationListContainer.js
+++ src/containers/LocationListContainer.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 
 import LocationList from './../components/LocationList';
 
 import { getWeatherCities, getCity } from './../reducers/';
-import { setSelectedCity, setWeather } from './../actions';
+import * as actions from './../actions';
+// import { setSelectedCity, setWeather } from './../actions';
+
 
 class LocationListContainer extends Component {
 
     componentDidMount(){
-        const { setWeather, setCity, cities, city} =  this.props;
+        const { setWeather, setSelectedCity, cities, city} =  this.props;
         
         setWeather(cities);
-        setCity(city);
+        setSelectedCity(city);
     }
 
     handleSelectionLocation = city => {
-        this.props.setCity(city);
+        this.props.setSelectedCity(city);
     }
 
     render() {
@@ -31,16 +34,20 @@ class LocationListContainer extends Component {
 }
 
 LocationListContainer.propTypes = {
-    setCity: PropTypes.func.isRequired,
+    setSelectedCity: PropTypes.func.isRequired,
+    setWeather: PropTypes.func.isRequired,
     cities:PropTypes.array.isRequired,
     citiesWeather:PropTypes.array,
     city:PropTypes.string.isRequired,
   }
   
-const mapDispatchToPropsActions = dispatch => ({
-    setCity: value => dispatch(setSelectedCity(value)),
-    setWeather: cities => dispatch(setWeather(cities)),
-});
+
+const mapDispatchToPropsActions = dispatch => bindActionCreators( actions, dispatch );
+
+// const mapDispatchToPropsActions = dispatch => ({
+//     setCity: value => dispatch(setSelectedCity(value)),
+//     setWeather: cities => dispatch(setWeather(cities)),
+// });
 
 const mapStateToProps = state => ({
     citiesWeather: getWeatherCities(state),
